b31ed4b2ea18e008961f70daa443f2037a027c59
Fabio <dahofcmmejia@gmail.com>
2023-03-21 22:48:13 -0400

    TDD has zero balance when is a newly opened account

---
+++ src/bank-account.js
@@ -0,0 +1,31 @@
+//
+// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class BankAccount {
+  constructor() {
+  }
+
+  open() {
+  }
+
+  close() {
+  }
+
+  deposit() {
+  }
+
+  withdraw() {
+  }
+
+  get balance() {
+    return 0;
+  }
+}
+
+export class ValueError extends Error {
+  constructor() {
+    super("Bank account error");
+  }
+}
